db5e1db8e8ce0bfd9ec4b966a61c4fa1b93ca826
qw1zzard <iq.pivnev@gmail.com>
2023-05-06 13:04:55 +0300

    Update GUI using place instead grid

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ EXT = 'Exterior'
 EXPL = 'Explicit'
 IMPL = 'Implicit'
 
+RELY = 0.0526
 
-def informer(task: StringVar, expl: StringVar, cent: Entry, rad: Entry,
-             circ: Entry, fun: Entry, inh: Entry) -> None:
+
+def informer(info: Label, task: StringVar, expl: StringVar, cent: Entry,
+             rad: Entry, circ: Entry, fun: Entry, inh: Entry) -> None:
     '''Collect the information, transfer it to the solver and build a graph'''
 
     task_s = task.get()
@@ -30,8 +32,11 @@ def informer(task: StringVar, expl: StringVar, cent: Entry, rad: Entry,
 
     u_rp, u_xy = runner(task_s, expl_s, cent_s, rad_s, circ_s, fun_s, inh_s)
 
-    Label(text=str(u_rp)).grid(row=16, column=1)
-    Label(text=str(u_xy)).grid(row=17, column=1)
+    u_rp_s = 'u(rho, phi) = ' + str(u_rp)
+    Label(text=u_rp_s).place(relx=.5, rely=17*RELY, anchor='center')
+
+    u_xy_s = 'u(x, y) = ' + str(u_xy)
+    Label(text=u_xy_s).place(relx=.5, rely=18*RELY, anchor='center')
 
     plot3d(u_xy)
 
@@ -41,58 +46,71 @@ def main() -> None:
 
     root = Tk()
     root.title('Solution of the Dirichlet-Neumann problem')
-    root.geometry('280x350')
+    root.geometry('600x400')
 
 
-    Label(text='Problem type:').grid(row=1, column=1)
+    info = Label(text='Use  ;  to separate input.')
+    info.place(relx=.5, rely=RELY, anchor='center')
+
+
+    Label(text='Problem type:').place(relx=.5, rely=2*RELY, anchor='center')
     task = StringVar(value=RING)
 
-    Radiobutton(text=RING, value=RING, variable=task).grid(row=2, column=0)
-    Radiobutton(text=INT, value=INT, variable=task).grid(row=2, column=1)
-    Radiobutton(text=EXT, value=EXT, variable=task).grid(row=2, column=2)
+    Radiobutton(text=RING, value=RING, variable=task) \
+        .place(relx=.25, rely=3*RELY, anchor='center')
+    Radiobutton(text=INT, value=INT, variable=task) \
+        .place(relx=.5, rely=3*RELY, anchor='center')
+    Radiobutton(text=EXT, value=EXT, variable=task) \
+        .place(relx=.75, rely=3*RELY, anchor='center')
 
 
-    Label(text='Circle parameters are given:').grid(row=3, column=1)
+    Label(text='Circle parameters are given:') \
+        .place(relx=.5, rely=4*RELY, anchor='center')
     expl = StringVar(value=EXPL)
 
-    Radiobutton(text=EXPL, value=EXPL, variable=expl).grid(row=4, column=0)
-    Radiobutton(text=IMPL, value=IMPL, variable=expl).grid(row=4, column=1)
+    Radiobutton(text=EXPL, value=EXPL, variable=expl) \
+        .place(relx=.33, rely=5*RELY, anchor='center')
+    Radiobutton(text=IMPL, value=IMPL, variable=expl) \
+        .place(relx=.66, rely=5*RELY, anchor='center')
 
 
-    Label(text='Circle center:').grid(row=5, column=1)
+    Label(text='Circle center:').place(relx=.5, rely=6*RELY, anchor='center')
     cent = Entry()
-    cent.grid(row=6, column=1)
+    cent.place(relx=.5, rely=7*RELY, anchor='center')
     cent.insert(0, '0 ; 0')
 
 
-    Label(text='Radius:').grid(row=7, column=1)
+    Label(text='Radius:').place(relx=.5, rely=8*RELY, anchor='center')
     rad = Entry()
-    rad.grid(row=8, column=1)
+    rad.place(relx=.5, rely=9*RELY, anchor='center')
     rad.insert(0, '1 ; 2')
 
 
-    Label(text='Circle equation:').grid(row=9, column=1)
+    Label(text='Circle equation:') \
+        .place(relx=.5, rely=10*RELY, anchor='center')
     circ = Entry()
-    circ.grid(row=10, column=1)
+    circ.place(relx=.5, rely=11*RELY, anchor='center', relwidth=.33)
     circ.insert(0, 'x^2+a*x+y^2+b*y=c ; x^2+a*x+y^2+b*y=c')
 
 
-    Label(text='Function on boundary:').grid(row=11, column=1)
+    Label(text='Function on boundary:') \
+        .place(relx=.5, rely=12*RELY, anchor='center')
     fun = Entry()
-    fun.grid(row=12, column=1)
+    fun.place(relx=.5, rely=13*RELY, anchor='center', relwidth=.33)
     fun.insert(0, 'x^2 ; 1/32*x^2+1/8')
 
 
-    Label(text='Non-homogeneous:').grid(row=13, column=1)
+    Label(text='Non-homogeneous:') \
+        .place(relx=.5, rely=14*RELY, anchor='center')
     inh = Entry()
-    inh.grid(row=14, column=1)
+    inh.place(relx=.5, rely=15*RELY, anchor='center', relwidth=.33)
     inh.insert(0, '0')
 
 
     solve_btn = Button(text='Solve problem',
-                       command=partial(informer, task, expl, cent,
+                       command=partial(informer, info, task, expl, cent,
                                        rad, circ, fun, inh))
-    solve_btn.grid(row=15, column=1)
+    solve_btn.place(relx=.5, rely=16*RELY, anchor='center')
 
 
     root.mainloop()

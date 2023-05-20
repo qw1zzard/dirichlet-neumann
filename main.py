715f56dd554cd343d577593481fbfdb0e96ff6f1
qw1zzard <iq.pivnev@gmail.com>
2023-05-21 01:31:51 +0300

    Solve the inhomogeneity and refactor

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 '''There's a lot of spaghetti code. Sorry.'''
 
 from functools import partial
-from tkinter import Tk, Label, StringVar
-from tkinter.ttk import Radiobutton, Entry, Button
-from sympy.plotting.plot import plot3d
+from tkinter import Label, StringVar, Tk
+from tkinter.ttk import Button, Entry, Radiobutton
+
 from sympy import init_printing
+from sympy.plotting.plot import plot3d
 
 from stuff import runner
 
-
 init_printing()
-
 
 RING = 'Ring'
 INT = 'Interior'
@@ -19,7 +18,7 @@ EXT = 'Exterior'
 EXPL = 'Explicit'
 IMPL = 'Implicit'
 
-RELY = 1/19.0
+RELY = 1/21
 
 
 def informer(info: Label, task: StringVar, expl: StringVar, cent: Entry,
@@ -37,10 +36,10 @@ def informer(info: Label, task: StringVar, expl: StringVar, cent: Entry,
     u_rp, u_xy = runner(task_s, expl_s, cent_s, rad_s, circ_s, fun_s, inh_s)
 
     u_rp_s = 'u(rho, phi) = ' + str(u_rp)
-    Label(text=u_rp_s).place(relx=.5, rely=17*RELY, anchor='center')
+    Label(text=u_rp_s).place(relx=.5, rely=19*RELY, anchor='center')
 
     u_xy_s = 'u(x, y) = ' + str(u_xy)
-    Label(text=u_xy_s).place(relx=.5, rely=18*RELY, anchor='center')
+    Label(text=u_xy_s).place(relx=.5, rely=20*RELY, anchor='center')
 
     plot3d(u_xy)
 
@@ -50,15 +49,13 @@ def main() -> None:
 
     root = Tk()
     root.title('Solution of the Dirichlet-Neumann problem')
-    root.geometry('600x400')
-
+    root.geometry('600x450')
 
     info = Label(text='Use  ;  to separate input.')
     info.place(relx=.5, rely=RELY, anchor='center')
 
-
     Label(text='Problem type:').place(relx=.5, rely=2*RELY, anchor='center')
-    task = StringVar(value=RING)
+    task = StringVar()
 
     Radiobutton(text=RING, value=RING, variable=task) \
         .place(relx=.25, rely=3*RELY, anchor='center')
@@ -67,28 +64,24 @@ def main() -> None:
     Radiobutton(text=EXT, value=EXT, variable=task) \
         .place(relx=.75, rely=3*RELY, anchor='center')
 
-
     Label(text='Circle parameters are given:') \
         .place(relx=.5, rely=4*RELY, anchor='center')
-    expl = StringVar(value=EXPL)
+    expl = StringVar()
 
     Radiobutton(text=EXPL, value=EXPL, variable=expl) \
-        .place(relx=.33, rely=5*RELY, anchor='center')
+        .place(relx=1/3, rely=5*RELY, anchor='center')
     Radiobutton(text=IMPL, value=IMPL, variable=expl) \
-        .place(relx=.66, rely=5*RELY, anchor='center')
-
+        .place(relx=2/3, rely=5*RELY, anchor='center')
 
     Label(text='Circle center:').place(relx=.5, rely=6*RELY, anchor='center')
     cent = Entry()
-    cent.place(relx=.5, rely=7*RELY, anchor='center')
-    cent.insert(0, '0 ; 0')
-
+    cent.place(relx=.5, rely=7*RELY, anchor='center', relwidth=.1)
+    cent.insert(0, 'x0 ; y0')
 
     Label(text='Radius:').place(relx=.5, rely=8*RELY, anchor='center')
     rad = Entry()
-    rad.place(relx=.5, rely=9*RELY, anchor='center')
-    rad.insert(0, '1 ; 2')
-
+    rad.place(relx=.5, rely=9*RELY, anchor='center', relwidth=.1)
+    rad.insert(0, 'r1 ; r2')
 
     Label(text='Circle equation:') \
         .place(relx=.5, rely=10*RELY, anchor='center')
@@ -96,26 +89,24 @@ def main() -> None:
     circ.place(relx=.5, rely=11*RELY, anchor='center', relwidth=.4)
     circ.insert(0, 'x^2+a*x+y^2+b*y=c ; x^2+a*x+y^2+b*y=c')
 
-
     Label(text='Function on boundary:') \
         .place(relx=.5, rely=12*RELY, anchor='center')
     fun = Entry()
     fun.place(relx=.5, rely=13*RELY, anchor='center', relwidth=.4)
-    fun.insert(0, 'x^2 ; 1/32*x^2+1/8')
+    fun.insert(0, 'f1 ; f2')
 
-
-    Label(text='Non-homogeneous:') \
+    Label(text='Inhomogeneity:') \
         .place(relx=.5, rely=14*RELY, anchor='center')
     inh = Entry()
-    inh.place(relx=.5, rely=15*RELY, anchor='center', relwidth=.4)
+    inh.place(relx=.5, rely=15*RELY, anchor='center', relwidth=.2)
     inh.insert(0, '0')
 
+    
 
     solve_btn = Button(text='Solve problem',
                        command=partial(informer, info, task, expl, cent,
                                        rad, circ, fun, inh))
     solve_btn.place(relx=.5, rely=16*RELY, anchor='center')
-
 
     root.mainloop()
 
--- stuff.py
+++ stuff.py
@@ -2,9 +2,11 @@
 
 from math import comb
 from re import compile, match
-from sympy import Expr, Float, Pow, symbols, simplify, solve, expand
-from sympy import exp, acos, sqrt, ln, sin, cos, preorder_traversal
 
+from sympy import (Add, Expr, Float, Mul, Pow, acos, cos, exp, expand,
+                   integrate, ln, preorder_traversal, simplify, sin, solve,
+                   sqrt, symbols)
+from sympy.abc import phi, rho, t, x, y
 
 RING = 'Ring'
 INT = 'Interior'
@@ -15,23 +17,20 @@ IMPL = 'Implicit'
 
 
 def circle_input(circle: str) -> tuple:
-    '''Calculation of center and radius from the string with
+    '''Calculate a center and radius from the string with
     the circle equation like x^2 + a*x + y^2 + b*y = c'''
 
-    # pattern = r'^x\^2([+-](\d+(?:\.\d+)?\*?)?x)?\+y\^2([+-](\d+(?:\.\d+)?\*?)?y)?([+-]?\d+(?:\.\d+)?)?=\d+(?:\.\d+)?$'
-    # pattern = compile(pattern)
+    pattern = r'^x\^2([+-](\d+(?:\.\d+)?\*?)?x)?\+y\^2([+-](\d+(?:\.\d+)?\*?)?y)?=[+-]?\d+(?:\.\d+)?$'
+    pattern = compile(pattern)
+    match_flg = match(pattern, circle)
+    if not match_flg:
+        raise ValueError('Invalid circle equation: ' + circle)
 
-    # match_flg = match(pattern, circle)
-    # if not match_flg:
-    #     raise ValueError('Invalid circle equation: ' + circle)
-
-
-    x, y = symbols('x y')
     lhs, rhs = circle.split('=')
     equation = simplify((simplify(lhs) - simplify(rhs)))
 
-    a = equation.coeff(x, 1) / 2
-    b = equation.coeff(y, 1) / 2
+    a = equation.coeff(x, 1) / 2.0
+    b = equation.coeff(y, 1) / 2.0
     c = - equation.coeff(x, 0).coeff(y, 0)
 
     x_0, y_0 = -a, -b
@@ -40,19 +39,17 @@ def circle_input(circle: str) -> tuple:
     return x_0, y_0, radius
 
 
-def humbler(function: Expr) -> Expr:
-    '''Decrease the degree of cosines and sines'''
+def humbler(function: str|Expr, x_0=0.0, y_0=0.0) -> Expr:
+    '''Simplify a function and decrease the degree of cosines and sines'''
 
-    phi = symbols('phi')
+    if isinstance(function, str):
+        function = expand(simplify(function).subs([(x, x_0 + rho * cos(phi)),
+                                                   (y, y_0 + rho * sin(phi))]))
 
-    cos_deg, sin_deg = set(), set()
-    for expr in preorder_traversal(function):
-        if isinstance(expr, Pow):
-            if isinstance(expr.args[0], cos):
-                cos_deg.add(expr.args[1])
-
-            elif isinstance(expr.args[0], sin):
-                sin_deg.add(expr.args[1])
+    cos_deg = {expr.args[1] for expr in preorder_traversal(function) \
+                if isinstance(expr, Pow) and isinstance(expr.args[0], cos)}
+    sin_deg = {expr.args[1] for expr in preorder_traversal(function) \
+                if isinstance(expr, Pow) and isinstance(expr.args[0], sin)}
 
     for n in cos_deg:
         sum_cos = 0
@@ -85,52 +82,69 @@ def humbler(function: Expr) -> Expr:
     return expand(function).rewrite(exp).expand().rewrite(cos).expand()
 
 
-def function_input(x_0: float, y_0: float, fun_entr: str) -> Expr:
-    '''Simplifying a function obtained in a string'''
+def inhomogeneity(x_0: float, y_0: float, inh_s: str) -> Expr | int:
+    '''Solve the inhomogeneity in the equation, give a partial solution'''
 
-    x, y, rho, phi = symbols('x, y, rho, phi')
+    if inh_s == '0':
+        return 0
 
-    function = expand(simplify(fun_entr).subs([(x, x_0 + rho * cos(phi)),
-                                               (y, y_0 + rho * sin(phi))]))
+    inh = humbler(inh_s, x_0, y_0)
 
-    return humbler(function)
+    if isinstance(inh, Add):
+        u_arr = list(inh.args)
+    else:
+        u_arr = [inh]
+
+    for i, arg in enumerate(u_arr):
+        mul = arg.args
+        f_r = Mul(mul[0], mul[1]) if len(mul) == 3 else mul[0]
+        g_p = mul[-1]
+
+        if g_p.args[0].args:
+            k = g_p.args[0].args[0]
+        else:
+            k = 1
+
+        f_r = f_r.subs(rho, exp(rho))
+
+        u = (exp(k*t) * integrate(exp(rho * (2-k)) * f_r, (rho, 1, t))
+             - exp(-k*t) * integrate(exp(rho * (2+k)) * f_r, (rho, 1, t))) \
+            / (2*k)
+
+        u = u.expand()
+        u -= (exp(k*t) * u.coeff(exp(k*t)) + exp(-k*t) * u.coeff(exp(-k*t)))
+
+        u = humbler(u.subs(t, ln(rho)))
+        u_arr[i] = u * g_p
+
+    return Add(*u_arr)
 
 
-def solver(task: str, rad_1: float, fun_1: Expr, inh: Expr,
+def solver(task: str, rad_1: float, fun_1: Expr,
            rad_2=0.0, fun_2=Expr()) -> Expr:
-    '''Solving the problem according to all obtained conditions'''
+    '''Solve the problem according to all obtained conditions'''
 
-    rho, phi, c_0, c_ln  = symbols('rho phi c_0 c_ln')
+    c_0, c_ln = symbols('c_0 c_ln')
     u = c_0
     c_arr = {c_0}
     if task == RING:
         u += c_ln * ln(rho)
         c_arr.add(c_ln)
 
-    cos_args, sin_args = set(), set()
-    for expr in preorder_traversal(fun_1 + fun_2):
-        if isinstance(expr, cos):
-            if expr.args[0].args:
-                cos_args.add(expr.args[0].args[0])
-            else: cos_args.add(1)
-
-        elif isinstance(expr, sin):
-            if expr.args[0].args:
-                sin_args.add(expr.args[0].args[0])
-            else: sin_args.add(1)
-
+    cos_args = {expr.args[0].args[0] if expr.args[0].args else 1 \
+                for expr in (fun_1 + fun_2).atoms(cos)}
+    sin_args = {expr.args[0].args[0] if expr.args[0].args else 1 \
+                for expr in (fun_1 + fun_2).atoms(sin)}
 
     for i in cos_args:
         c_c0, c_c1 = symbols(f'c_c0_{i} c_c1_{i}')
         if task == RING:
-            c_arr.update([c_c0, c_c1])
+            c_arr.update({c_c0, c_c1})
             u += c_c0*(rho**i) * cos(i*phi) \
                 + c_c1*(rho**-i) * cos(i*phi)
-
         elif task == INT:
             c_arr.add(c_c0)
             u += c_c0*(rho**i) * cos(i*phi)
-
         else:
             c_arr.add(c_c1)
             u += c_c1*(rho**-i) * cos(i*phi)
@@ -138,14 +152,12 @@ def solver(task: str, rad_1: float, fun_1: Expr, inh: Expr,
     for i in sin_args:
         c_s0, c_s1 = symbols(f'c_s0_{i} c_s1_{i}')
         if task == RING:
-            c_arr.update([c_s0, c_s1])
+            c_arr.update({c_s0, c_s1})
             u += c_s0*(rho**i) * sin(i*phi) \
                 + c_s1*(rho**-i) * sin(i*phi)
-
         elif task == INT:
             c_arr.add(c_s0)
             u += c_s0*(rho**i) * sin(i*phi)
-
         else:
             c_arr.add(c_s1)
             u += c_s1*(rho**-i) * sin(i*phi)
@@ -153,7 +165,6 @@ def solver(task: str, rad_1: float, fun_1: Expr, inh: Expr,
     u_1 = humbler(u.subs(rho, rad_1) - fun_1.subs(rho, rad_1))
     if task == RING:
         u_2 = humbler(u.subs(rho, rad_2) - fun_2.subs(rho, rad_2))
-
 
     u_10 = u_1.coeff(cos(phi), 0).coeff(sin(phi), 0)
     if task == RING:
@@ -164,19 +175,11 @@ def solver(task: str, rad_1: float, fun_1: Expr, inh: Expr,
         if task == RING:
             u_20 = u_20.coeff(cos(i*phi), 0).coeff(sin(i*phi), 0)
 
-    eq_arr = {u_10}
+    eq_arr = {u_10} | {u_1.coeff(cos(i*phi)) for i in cos_args} | \
+        {u_1.coeff(sin(i*phi)) for i in sin_args}
     if task == RING:
-        eq_arr.add(u_20)
-
-    for i in cos_args:
-        eq_arr.add(u_1.coeff(cos(i*phi)))
-        if task == RING:
-            eq_arr.add(u_2.coeff(cos(i*phi)))
-
-    for i in sin_args:
-        eq_arr.add(u_1.coeff(sin(i*phi)))
-        if task == RING:
-            eq_arr.add(u_2.coeff(sin(i*phi)))
+        eq_arr |= {u_20} | {u_2.coeff(cos(i*phi)) for i in cos_args} | \
+            {u_2.coeff(sin(i*phi)) for i in sin_args}
 
     c_sol = solve(eq_arr, c_arr)
     u = simplify(u.subs(c_sol))
@@ -184,52 +187,50 @@ def solver(task: str, rad_1: float, fun_1: Expr, inh: Expr,
     return u
 
 
-def runner(task: str, expl: str, cent: str, rad: str,
-           circ: str, fun: str, inh: str) -> tuple:
-    '''Running side functions with the task in mind'''
+def runner(task: str, expl: str, cent: str, rad_s: str,
+           circ: str, fun_s: str, inh_s: str) -> tuple:
+    '''Run side functions with the task in mind'''
 
     if task == RING:
         if expl == EXPL:
             x_0, y_0 = map(float, cent.split(';'))
-            rad_1, rad_2 = map(float, rad.split(';'))
+            rad_1, rad_2 = map(float, rad_s.split(';'))
         else:
             circ_1, circ_2 = circ.split(';')
             x_0, y_0, rad_1 = map(float, circle_input(circ_1))
             x_0, y_0, rad_2 = map(float, circle_input(circ_2))
 
-        fun_1, fun_2 = fun.split(';')
-        fun_1 = function_input(x_0, y_0, fun_1)
-        fun_2 = function_input(x_0, y_0, fun_2)
-        inh_x = function_input(x_0, y_0, inh)
+        fun_1, fun_2 = fun_s.split(';')
+        fun_1 = humbler(fun_1, x_0, y_0)
+        fun_2 = humbler(fun_2, x_0, y_0)
+        u_par = inhomogeneity(x_0, y_0, inh_s)
 
-        u_rp = solver(task, rad_1, fun_1, inh_x,
-                      rad_2=rad_2, fun_2=fun_2)
+        u_rp = solver(task, rad_1, fun_1-u_par,
+                      rad_2=rad_2, fun_2=fun_2-u_par) + u_par
     else:
         if expl == EXPL:
             x_0, y_0 = map(float, cent.split(';'))
-            rad_ex = float(rad.replace(' ', ''))
+            rad = float(rad_s.replace(' ', ''))
         else:
-            x_0, y_0, rad_ex = map(float, circle_input(circ))
+            x_0, y_0, rad = map(float, circle_input(circ))
 
-        fun_x = function_input(x_0, y_0, fun)
-        inh_x = function_input(x_0, y_0, inh)
+        fun = humbler(fun_s, x_0, y_0)
+        u_par = inhomogeneity(x_0, y_0, inh_s)
 
-        u_rp = solver(task, rad_ex, fun_x, inh_x)
+        u_rp = solver(task, rad, fun-u_par) + u_par
 
-    x, y, rho, phi = symbols('x y rho phi')
     rho_new = sqrt((x - x_0)**2 + (y - y_0**2))
     phi_new = acos((x - x_0) / rho_new)
 
     u_xy = simplify(u_rp.subs([(rho, rho_new), (phi, phi_new)]))
 
+    u_rp_args = [arg for arg in preorder_traversal(u_rp) \
+                 if isinstance(arg, Float)]
+    u_rp = u_rp.subs([(arg, round(arg, 4)) for arg in u_rp_args])
 
-    for arg in preorder_traversal(u_rp):
-        if isinstance(arg, Float):
-            u_rp = u_rp.subs(arg, round(arg, 4))
-
-    for arg in preorder_traversal(u_xy):
-        if isinstance(arg, Float):
-            u_xy = u_xy.subs(arg, round(arg, 4))
+    u_xy_args = [arg for arg in preorder_traversal(u_xy) \
+                 if isinstance(arg, Float)]
+    u_xy = u_xy.subs([(arg, round(arg, 4)) for arg in u_xy_args])
 
     u_rp, u_xy = map(humbler, [u_rp, u_xy])
 
